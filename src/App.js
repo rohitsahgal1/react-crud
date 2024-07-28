4df8b22d398666910dd8e1b92387d78dd0c1cf1b
rsehgal088@gmail.com <rsehgal087@gmail.com>
2024-07-28 11:59:29 +0530

    source code of react-crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+    {/* </div>
+    <div> */}
+      <Routes>
+      <Route path='/create' element={<Create/>} />
+      <Route path='/read' element={<Read/>} />
+      <Route path='/update' element={<Update/>} />
+      </Routes>
     </div>
+     
+    </>
   );
 }
 
